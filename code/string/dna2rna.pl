f2b4e1e8fbceda5df16ec0474977200888c940d5
Rafael Silveira <dev.silveira@yahoo.com>
2013-07-16 13:36:04 -0300

    updating dna2rna

--- code/string/dna2rna.pl
+++ code/string/dna2rna.pl
@@ -3,20 +3,7 @@
 use strict;
 use warnings;
 
-open IFILE, '<', '../../data/string/dna2rna.txt' or die('File not found.');
-open OFILE, '>', '../../output/string/dna2rna.txt' or die ('Can\'t create file.');
-
-my $output = '';
-
-while (my $line = <IFILE>) {
-  chomp $line;  
+while (my $line = <>) {
   $line =~ s/T/U/gi;
-  $output .= "$line\n";
+  print $line;
 }
-
-chomp $output;
-
-print OFILE $output;
-
-close IFILE;
-close OFILE;
