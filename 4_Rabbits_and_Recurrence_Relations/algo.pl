c527383a4e9b495c0cbd74c58040afa38681ad89
Rafael Silveira <dev.silveira@yahoo.com>
2013-07-18 16:43:26 -0300

    modificando estrutura

---
+++ 4_Rabbits_and_Recurrence_Relations/algo.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub fibonacci {
+  my ($n, $k, @stack) = @_;
+  
+  while (scalar @stack < $n) {
+    push @stack, int($stack[-1]) + int($stack[-2]) * $k;
+  }
+  
+  return $stack[-1];
+}
+
+while (my $line = <>) {
+  my ($n, $k) = split ' ', $line;
+  print fibonacci( $n, $k, (1, 1) );
+}
